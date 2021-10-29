a3799fac25e48397f394d5446d7811eb0b1c0c1f
phamdat311 <90233954+phamdat311@users.noreply.github.com>
2021-10-29 09:27:42 +0700

    Add bookTour, Process_bookTour

---
+++ src/process_bookTour.php
@@ -0,0 +1,27 @@
+<?php
+    if (isset($_POST['btnSuccess'])) {
+        $Name = $_POST['txtName'];
+        $Mail = $_POST['txtEmail'];
+        $Phone = $_POST['txtPhone'];
+        $Address = $_POST['txtAddress'];
+        $Note = $_POST['txtNote'];
+        $Room = $_POST['txtRoom'];
+        $id = 'abc';
+        $conn = mysqli_connect('localhost', 'root', '', 'db_travel');
+        if (!$conn) {
+            die("Không thể kết nối !");
+        }
+        $sql = "INSERT INTO tour_user(user_id, user_full, user_phone, user_email, user_address, user_note, user_number_room) 
+            VALUES ('$id','$Name','$Mail','$Phone','$Address','$Note','$Room')";
+
+        echo $sql;
+        $result = mysqli_query($conn, $sql);
+        if($result == true){
+            echo "Success";
+            //header("location: index.php");
+        }
+        else{
+            echo "Eroor";
+        }
+    }
+?>
