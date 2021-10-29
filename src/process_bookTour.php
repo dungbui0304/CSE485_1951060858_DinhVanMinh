183722b26712a0c5544f515fa7c5c331818c18ae
dvminh2ob <dinhvanminh2ob@gmail.com>
2021-10-29 09:37:48 +0700

    Update proess_bookTour.php

--- src/process_bookTour.php
+++ src/process_bookTour.php
@@ -6,13 +6,12 @@
         $Address = $_POST['txtAddress'];
         $Note = $_POST['txtNote'];
         $Room = $_POST['txtRoom'];
-        $id = 'abc';
         $conn = mysqli_connect('localhost', 'root', '', 'db_travel');
         if (!$conn) {
             die("Không thể kết nối !");
         }
-        $sql = "INSERT INTO tour_user(user_id, user_full, user_phone, user_email, user_address, user_note, user_number_room) 
-            VALUES ('$id','$Name','$Mail','$Phone','$Address','$Note','$Room')";
+        $sql = "INSERT INTO tour_user( user_full, user_phone, user_email, user_address, user_note, user_number_room) 
+            VALUES ('$Name','$Mail','$Phone','$Address','$Note','$Room')";
 
         echo $sql;
         $result = mysqli_query($conn, $sql);
@@ -21,7 +20,7 @@
             //header("location: index.php");
         }
         else{
-            echo "Eroor";
+            echo "Error";
         }
     }
 ?>
