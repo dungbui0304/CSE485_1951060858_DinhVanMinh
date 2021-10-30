e66d2be16e7efd2b59451e04757aee86bdc03dfe
dvminh2ob <dinhvanminh2ob@gmail.com>
2021-10-30 12:01:03 +0700

    Add header.php

---
+++ src/header.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assest/fonts/fontawesome-free-5.15.4/fontawesome-free-5.15.4-web/css/all.min.css">
+    <link rel="stylesheet" href="./assest/css/style.css">
+    <title>Travel Web</title>
+  </head>
+<body>
+<!-- Header -->
+
+<nav class="navbar navbar-expand-lg navbar-light sticky-top">
+    <div class="container-fluid">
+        <a href="#" class="navbar-brand">
+            <img src="" height="60px" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+            <li class="nav-item text-white">
+                <a class="nav-link text-white active" aria-current="page" href="index.php">
+                <i class="fas fa-home"></i>
+                    TRANG CHỦ
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="tour.php">
+                <i class="fas fa-plane-arrival"></i>TOUR</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">COMBO</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">KHÁCH SẠN</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#"><i class="fas fa-user"></i>ĐĂNG NHẬP</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+</html>
+
