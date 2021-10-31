5b7d7fd29ee0db34f59272086d1c03427c94cb10
dungbui0304 <buithedung0304@gmail.com>
2021-10-31 23:14:06 +0700

    add folder trang admin

---
+++ src/admin/dashboard.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assest/css/style.css">
+    <link rel="stylesheet" href="./assest/fonts/fontawesome-free-5.15.4/fontawesome-free-5.15.4-web/css/all.min.css">
+    <title>Admin Travel</title>
+</head>
+
+<body>
+    <div class="main">
+        <?php include('include/sidebarmenu.php') ?>
+        <div class="right-content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <?php include('include/header.php')?>
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="">Home</a> <i class="fa fa-angle-right"></i></li>
+                        </ol>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-3 mb">
+                        <div class="box box1">
+                            <i class="fas fa-user"></i>
+                            <h1>User</h1>
+                            <span>7</span>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-3 mb">
+                        <div class="box box2">
+                        <i class="fas fa-spa"></i>
+                            <h1>Booking</h1>
+                            <span>9</span>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-3 mb">
+                        <div class="box box3">
+                        <i class="fas fa-glass-martini"></i>
+                            <h1>Tour</h1>
+                            <span>4</span>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-3 mb">
+                        <div class="box">
+
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-3 mb">
+                        <div class="box box5">
+                        <i class="fas fa-spa"></i>
+                        <h1>hi</h1>
+                        <span>8</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
