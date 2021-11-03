40b2673f096683d16deeb24f71137673c8df5b06
dvminh2ob <dinhvanminh2ob@gmail.com>
2021-11-03 23:55:52 +0700

    Update bookTour.php

--- src/bookTour.php
+++ src/bookTour.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="./assest/fonts/fontawesome-free-5.15.4/fontawesome-free-5.15.4-web/css/all.min.css">
-  
+
     <link rel="stylesheet" href="./assest/css/style.css">
     <title>Travel Web</title>
 </head>
